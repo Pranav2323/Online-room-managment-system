4c14c8679b2ee87c17466437fb80a0b206e97bf7
Pranav2323 <61003259+Pranav2323@users.noreply.github.com>
2020-05-16 20:10:50 +0530

    Add files via upload

--- Invoice.java
+++ Invoice.java
@@ -2,26 +2,19 @@ public class Invoice {
 
 public int CalBill(int days, String type) {
 	int bill=0;
-	if(day>=0)
+	if (type.compareTo("single")==0)
 	{
-		if (type.compareTo("single")==0)
-		{
-			bill=3000*days;
-		}
-		else if(type.compareTo("twin bed")==0)
-		{
-			bill=4000*days;
-		}
-		else if(type.compareTo("suite")==0)
-		{
-			bill=12000*days;
-		}
-	
-		return bill;
+		bill=2500*days;
 	}
-	else
+	else if(type.compareTo("twin bed")==0)
 	{
-		return -1;
+		bill=3500*days;
+	}
+	else if(type.compareTo("suite")==0)
+	{
+		bill=10000*days;
 	}
 	
+	return bill;
+	}
 }
